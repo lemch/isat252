18de93e1354c965c9abb615257b98ab2cb651d43
Lizzie Emch <60358256+lemch@users.noreply.github.com>
2020-05-04 17:06:36 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,21 @@
+def Variables():
+    print("Hello")
+
+Variables()
+
+def Greeting(name):
+    print("Hi" + name + "!")
+
+Greeting("Lizzie")
+
+def Add(num1, num2):
+    print(num1 + num2)
+
+Add(10,15)
+
+def returnAdd(num1, num2):
+    return(num1 +num2)
+
+sum = returnAdd(12,34)
+
+print(sum)
